18d30f36b904027c8638af79de9f98597d05358c
Maximilian Herold <herold@emjot.de>
2026-08-13 16:47:45 +0200

    Fix more RSpec cops
    - RSpec/StubbedMock
    - RSpec/HookArgument
    - RSpec/MessageSpies
    - RSpec/InstanceVariable

--- spec/paperclip_compression/png_spec.rb
+++ spec/paperclip_compression/png_spec.rb
@@ -1,30 +1,31 @@
 # frozen_string_literal: true
 
 RSpec.describe PaperclipCompression::Png do
-  before(:each) do
+  let(:file) do
     path = File.join(PaperclipCompression.root, 'spec', 'support', 'test.png')
-    @file = File.new(path, 'rb')
-    @key = PaperclipCompression::Png::KEY
+    File.new(path, 'rb')
   end
 
-  after(:each) do
-    @file.close
+  after do
+    file.close
   end
 
   it 'processes the file via embedded binary' do
-    result = described_class.new(@file, false, whiny: true).make
+    result = described_class.new(file, false, whiny: true).make
 
     expect(File.size(result.path)).to be > 0
-    expect(File.size(result.path)).to be <= File.size(@file.path)
+    expect(File.size(result.path)).to be <= File.size(file.path)
   end
 
   it 'does not process if command exists but config.process_file? is false' do
-    expect(PaperclipCompression::Config).to receive(:create_with_fallbacks).and_return(
+    allow(PaperclipCompression::Config).to receive(:create_with_fallbacks).and_return(
       instance_double(PaperclipCompression::Config, process_file?: false, command: 'command')
     )
 
-    expect(Paperclip).not_to receive(:run)
-    described_class.new(@file, false).make
+    allow(Paperclip).to receive(:run)
+    described_class.new(file, false).make
+
+    expect(Paperclip).not_to have_received(:run)
   end
 
   it 'uses config command and options' do
@@ -39,7 +40,9 @@ RSpec.describe PaperclipCompression::Png do
       }
     }
 
-    expect(Paperclip).to receive(:run).with('abc', 'xyz -clobber :src_path -out :dst_path', anything)
-    described_class.new(@file, false, options).make
+    allow(Paperclip).to receive(:run)
+    described_class.new(file, false, options).make
+
+    expect(Paperclip).to have_received(:run).with('abc', 'xyz -clobber :src_path -out :dst_path', anything)
   end
 end
